387c31fe489dfe97b37d84e27b351a9c984d1156
Ramya-Kaveramma <112173125+Ramya-Kaveramma@users.noreply.github.com>
2022-09-08 11:21:26 +0530

    Add files via upload

---
+++ OOPS_Program/Abstraction/Verification.java
@@ -0,0 +1,29 @@
+import java.util.*;  
+
+class LoginPage{  
+     String Username; 
+     String password; 
+    }  
+    class Verification extends LoginPage{  
+     String otp;
+
+
+     public static void main(String args[]){  
+       Scanner sc= new Scanner(System.in);
+       Verification v = new Verification ();
+
+  
+       System.out.println("Enter the username"); 
+       System.out.println("Enter the password");
+       System.out.println("Enter the otp");
+
+
+       String Username = u.nextLine(); 
+       String password = p.nextLine(); 
+       String otp = o.nextLine(); 
+
+       System.out.println("The username, password,and otp is:"+u.bonus);  
+       System.out.println("The password is:"+p.bonus);  
+       System.out.println("The otp is:"+o.bonus);  
+    }  
+    }  
